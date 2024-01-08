22ee07a1a89707b3a2e8b37ad1900e5b9bcf2f39
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2024-01-08 15:38:54 +0700

    move RPG folder to inside javascript-algorithms and make function buy weapon done

---
+++ javascript-algorithms/RPG_dragon-repeller/assets/js/index.js
@@ -0,0 +1,120 @@
+// initialize Variables
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let inventory = ["stick"];
+let fighting;
+let monsterHealth;
+const weapons = [
+  {
+    name: "stick",
+    power: 5,
+  },
+  {
+    name: "dagger",
+    power: 30,
+  },
+  {
+    name: "claw hammer",
+    power: 50,
+  },
+  {
+    name: "sword",
+    power: 100,
+  },
+];
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says Store.",
+  },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters.",
+  },
+];
+
+// initialize DOM variable
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpTextElement = document.querySelector("#xpText");
+const healthTextElement = document.querySelector("#healthText");
+const goldTextElement = document.querySelector("#goldText");
+const monsterStatsElement = document.querySelector("#monsterStats");
+const monsterNameElement = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+// initialize buttons
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+// function declare
+function goStore() {
+  update(locations[1]);
+}
+
+function goTown() {
+  update(locations[0]);
+}
+
+function update(location) {
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
+}
+
+function goCave() {
+  update(locations[2]);
+}
+
+// function inner Cave
+function fightSlime() {}
+function fightBeast() {}
+
+// function inner go Store
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    // update game screen
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
+
+function buyWeapon() {
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    // update game screen
+    goldText.innerText = gold;
+    text.innerText = `You now have a new weapon ${weapons[currentWeapon].name}`;
+  }
+}
+
+function fightDragon() {}
